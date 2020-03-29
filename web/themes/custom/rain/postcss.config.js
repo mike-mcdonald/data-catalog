e4d8e20b373bb1431bccd118c5d7000e7fbcb73e
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2020-03-29 14:14:01 -0700

    Upgrade purgecss

--- web/themes/custom/rain/postcss.config.js
+++ web/themes/custom/rain/postcss.config.js
@@ -3,10 +3,6 @@ const purgecss = require('@fullhuman/postcss-purgecss');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 
-// Custom PurgeCSS extractor for Tailwind that allows special characters in
-// class names.
-//
-// https://github.com/FullHuman/purgecss#extractor
 module.exports = ({ file, options, env }) => {
   return {
     plugins: [
@@ -20,8 +16,8 @@ module.exports = ({ file, options, env }) => {
           content: [
             'templates/**/*.html.twig',
             'layouts/**/*.html.twig',
-            'js/main.bundle.js',
-            './rain.theme'
+            'js/**/*.js',
+            'rain.theme'
           ],
           defaultExtractor: content => content.match(/[\w-/:()]+(?<!:)/g) || [],
           whitelistPatternsChildren: [/select2-container--rain$/]
