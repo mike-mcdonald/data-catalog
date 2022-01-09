2d927ae48b0fc20c1b3c55cef750986740d9f2bb
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2022-01-08 17:43:01 -0800

    feat: Upgrade OpenID Connect
    Updates to 2.0 version, refactors Azure plugin, adding keys instead of custom secret reader.

--- web/modules/custom/portland/src/Routing/RouteSubscriber.php
+++ web/modules/custom/portland/src/Routing/RouteSubscriber.php
@@ -10,7 +10,8 @@ use Drupal\Core\Routing\RoutingEvents;
 /**
  * Listens to the dynamic route events.
  */
-class RouteSubscriber extends RouteSubscriberBase {
+class RouteSubscriber extends RouteSubscriberBase
+{
   /**
    * The logger channel factory.
    *
@@ -24,30 +25,27 @@ class RouteSubscriber extends RouteSubscriberBase {
    * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
    *   The logger channel factory.
    */
-  public function __construct(LoggerChannelFactoryInterface $logger_factory) {
+  public function __construct(LoggerChannelFactoryInterface $logger_factory)
+  {
     $this->loggerFactory = $logger_factory;
   }
 
   /**
    * {@inheritdoc}
    */
-  protected function alterRoutes(RouteCollection $collection) {
-    // only log in with an OpenID provider
-    if ($route = $collection->get('user.login')) {
-      $route->setDefault('_form', 'Drupal\openid_connect\Form\LoginForm');
-    }
+  protected function alterRoutes(RouteCollection $collection)
+  {
     // don't accept POSTs to a login route
-    if ($route = $collection->get('user.login.http')) {
-      $route->setRequirement('_access', 'FALSE');
+    if ($route = $collection->get("user.login.http")) {
+      $route->setRequirement("_access", "FALSE");
     }
     // don't allow password resets via Drupal
-    if($route = $collection->get('user.pass')) {
-      $route->setRequirement('_access', 'FALSE');
+    if ($route = $collection->get("user.pass")) {
+      $route->setRequirement("_access", "FALSE");
     }
     // don't accept POSTs to a password reset form
-    if($route = $collection->get('user.pass.http')) {
-      $route->setRequirement('_access', 'FALSE');
+    if ($route = $collection->get("user.pass.http")) {
+      $route->setRequirement("_access", "FALSE");
     }
   }
-
 }
